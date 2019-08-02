bc80f401b5f2f2ad818bdb8799920ddfb2d0c473
Armando Morales <amorales944@gmail.com>
2019-08-02 14:40:14 -0500

    added path dependancy

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-require("dotenv").config();
 const express = require("express");
-
+const path = require("path");
 const app = express();
-
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
-const db = require("./models");
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("chess/public"));
+  }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./chess/public/index.html"));
+  });
+
+// app.use(express.static("public"));
 // require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 //remember to change force: true to force: false
-db.sequelize.sync({ force: false }).then(function() {
+// db.sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
       });
-  });
+//   });
