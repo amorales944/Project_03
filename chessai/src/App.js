23cf6da7f5aa91b002ae1f10e2f696beb87130b8
csporto23 <Csporto23@gmail.com>
2019-08-03 11:17:34 -0500

    fixed npm and yarn lock

--- chessai/src/App.js
+++ chessai/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Multiplayer from "./pages/Multiplayer";
 import Homepage from "./pages/Homepage";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar />
         <Wrapper>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/Homepage" component={Homepage} />
