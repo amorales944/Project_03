49247df39cb14cf8f98375d55a1e8c2f0a7b4691
csporto23 <Csporto23@gmail.com>
2019-08-10 11:10:59 -0500

    adding chess react app

---
+++ chess/src/components/pages/Singleplayer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Multiplayer from './Multiplayer'
+
+
+
+class Singleplayer extends Component {
+
+
+  render() {
+    return (
+      <div>
+    <Multiplayer />
+      </div>
+    );
+  }
+}
+
+export default Singleplayer;
